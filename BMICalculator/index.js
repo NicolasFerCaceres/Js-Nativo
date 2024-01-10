ee9071f057e477413c723d938e6f63b19b06a5c3
nico <nicofernando895@gmail.com>
2024-01-10 17:54:33 -0300

    Is nearly finished

--- BMICalculator/index.js
+++ BMICalculator/index.js
@@ -9,63 +9,97 @@ const bmiForm = document.getElementById('bmiForm');
 const weightInput = document.getElementById('weight');
 const heightInput = document.getElementById('height');
 const bmiP = document.getElementById('bmiText');
+const formElem = document.querySelectorAll('.bmiInput');
+const bmiBtn = document.getElementById('submitBtn')
+const heightTwo = document.getElementById('heightTwo');
+const category = document.getElementById('catSelector');
+const weightRange = document.getElementById('weightRange');
+const calWeightBtn = document.getElementById('calWeightBtn');
+const formWeight = document.getElementById('formWeight');
 
-
-bmiForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const weight = weightInput.value;
-    const height = heightInput.value;
-    const bmi = Math.round((weight / (height * height)) * 100) / 100;
-
-    if (weight === '' || height === '') {
-        showError('Should complete both fields to know your BMI.');
-        return;
-    } else if (!/^\d+$/.test(weight) || !/^\d+$/.test(height)) {
-        showError('Only numbers, please.');
-        return;
+function calcBmi(weight, height) {
+    console.log(weight, height);
+    const bmi = weight / (height*height);
+    if(weight === '' || height === '') {
+        showError('You need to fill the fields' , bmiP)
+        return
+    } else if(isNaN(bmi)) {
+        showError('Only numbers please', bmiP)
+        return
+    } else {
+        showMesaje('Your BMI is ' + bmi)
     }
+    bmiForm.reset()
+    weightInput.focus()
+}
 
-    showBMI('Your BMI is: ' + bmi);
+// function showBMI
+function showError(message, component) {
+    component.style.color = 'red';
+    component.textContent = message;
+    setTimeout(function() {
+        component.classList.toggle('visible');
+    }, 300);
+    setTimeout(function(){
+        component.classList.toggle('visible');
+    }, 2000);
+}
+
+function showMesaje(message, component) {
+    component.style.color = 'green';
+    component.textContent = message;
+    setTimeout(function() {
+        component.classList.toggle('visible');
+    }, 300);
+
+    setTimeout(function(){
+        component.classList.toggle('visible');
+    }, 5000);
+}
+
+bmiBtn.addEventListener('click', () => {calcBmi(weightInput.value, heightInput.value)});
+
+formElem.forEach((element, index) => {
+    element.addEventListener('keydown', function(event){
+        if (event.key === 'Enter') {
+            if(formElem.length - 1  === index){
+                calcBmi(weightInput.value, heightInput.value)
+            } else {
+                heightInput.focus(); 
+            }
+        }
+    })
 });
 
-function showError(message) {
-    bmiP.textContent = message;
-    bmiP.style.color = 'red';
-    bmiP.style.opacity = '0';
-    bmiP.style.transition = 'opacity 0.5s ease'; // Transition for color change
-}
-
-function showBMI(message) {
-    bmiP.textContent = message;
-    bmiP.style.color = 'green';
-    bmiP.style.opacity = '0';
-    bmiP.style.transition = 'opacity 0.5s ease'; // Transition for color change
-}
-/* 
-function getRange(bmi) {
-    if(bmi < 18.5) {
-        console.log('Underweight(BMI below 18.5).| Your BMI is: ' + bmi);
-    } else if (bmi < 25) {
-        console.log('Normal(BMI 18.5-24.9).| Your BMI is: ' + bmi);
-    } else if(bmi < 30) {     
-        console.log('Overweight(BMI 25-29.9).| Your BMI is: ' + bmi);
-    }else if(bmi < 35){
-        console.log('Obesity class I(BMI 30-34.9).| Your BMI is: ' + bmi);
-    }else if(bmi < 40) {
-        console.log('Obeseity class II(BMI 35-39.9).| Your BMI is: ' + bmi);
-    }else {
-        console.log('Obeseity class III(BMI biger than 40). | Your BMI is: ' + bmi)
-    }   
-} */
-
-/* 
-function getBmi(weight, height) {
-    const bmi = weight / (height * height);      
-    getRange(bmi)
+const bmiCategories = {
+    underweight: {min:0 , max: 18.5},
+    normal: {min: 18.5, max: 24.9 },
+    overweight: { min: 25, max: 29.9 },
+    obesityClassI: { min: 30, max: 34.9 },
+    obesityClassII: { min: 35, max: 39.9 },
+    obesityClassIII: {min:40, max: Number.POSITIVE_INFINITY }
 };
 
-getBmi(69, 1.73);
- */
+calWeightBtn.addEventListener('click', ()=> {
+    calWeight(heightTwo.value, category.value)
+})
 
 
+function calWeight(height, category) {
+if(height === ''){
+    showError('Please fill the height field', weightRange);
+}else if(isNaN(height * 2)){
+    showError('Only numbers please', heightTwo);
+} else if (bmiCategories.hasOwnProperty(category)) {
+        const minBmi = bmiCategories[category].min;
+        const maxBmi = bmiCategories[category].max;
+        const maxWeight = Math.round(height * height * maxBmi * 100) / 100;
+        const minWeight = Math.round(height * height * minBmi * 100) / 100;
+        if(minWeight === 0) {
+            showMesaje('max: ' + maxWeight + 'Kg', weightRange)
+        } else {
+            showMesaje('min: ' + minWeight + 'Kg' + ' | ' + 'max: ' + maxWeight + 'Kg', weightRange)
+        }
+    }    
+}
 
