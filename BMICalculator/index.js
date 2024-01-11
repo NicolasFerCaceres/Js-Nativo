ecbfc9cad3b79df13446ab0ca7a8b9991660e609
nico <nicofernando895@gmail.com>
2024-01-11 16:58:50 -0300

    Bmi Styling

--- BMICalculator/index.js
+++ BMICalculator/index.js
@@ -17,44 +17,16 @@ const weightRange = document.getElementById('weightRange');
 const calWeightBtn = document.getElementById('calWeightBtn');
 const formWeight = document.getElementById('formWeight');
 
-function calcBmi(weight, height) {
-    console.log(weight, height);
-    const bmi = weight / (height*height);
-    if(weight === '' || height === '') {
-        showError('You need to fill the fields' , bmiP)
-        return
-    } else if(isNaN(bmi)) {
-        showError('Only numbers please', bmiP)
-        return
-    } else {
-        showMesaje('Your BMI is ' + bmi)
-    }
-    bmiForm.reset()
-    weightInput.focus()
-}
+
+
 
 // function showBMI
 function showError(message, component) {
-    component.style.color = 'red';
     component.textContent = message;
-    setTimeout(function() {
-        component.classList.toggle('visible');
-    }, 300);
-    setTimeout(function(){
-        component.classList.toggle('visible');
-    }, 2000);
 }
 
 function showMesaje(message, component) {
-    component.style.color = 'green';
     component.textContent = message;
-    setTimeout(function() {
-        component.classList.toggle('visible');
-    }, 300);
-
-    setTimeout(function(){
-        component.classList.toggle('visible');
-    }, 5000);
 }
 
 bmiBtn.addEventListener('click', () => {calcBmi(weightInput.value, heightInput.value)});
@@ -67,17 +39,35 @@ formElem.forEach((element, index) => {
             } else {
                 heightInput.focus(); 
             }
+        } else {
+            bmiP.textContent = '';
         }
     })
 });
 
+function calcBmi(weight, height) {
+    console.log(weight, height);
+    const bmi =Math.round((weight / (height*height)) * 100) / 100;
+    if(weight === '' || height === '') {
+        showError('You need to fill the fields' , bmiP)
+        return
+    } else if(isNaN(bmi)) {
+        showError('Only numbers please', bmiP)
+        return
+    } else {
+        showMesaje('Your BMI is ' + bmi, bmiP )
+    }
+    bmiForm.reset()
+    weightInput.focus()
+}
+
 const bmiCategories = {
-    underweight: {min:0 , max: 18.5},
-    normal: {min: 18.5, max: 24.9 },
-    overweight: { min: 25, max: 29.9 },
-    obesityClassI: { min: 30, max: 34.9 },
-    obesityClassII: { min: 35, max: 39.9 },
-    obesityClassIII: {min:40, max: Number.POSITIVE_INFINITY }
+    Underweight: {min:0 , max: 18.5},
+    Normal: {min: 18.5, max: 24.9 },
+    Overweight: { min: 25, max: 29.9 },
+    'Obesity Class I': { min: 30, max: 34.9 },
+    'Obesity Class II': { min: 35, max: 39.9 },
+    'Obesity Class III': {min:40, max: Number.POSITIVE_INFINITY }
 };
 
 calWeightBtn.addEventListener('click', ()=> {
@@ -85,20 +75,27 @@ calWeightBtn.addEventListener('click', ()=> {
 })
 
 
+formWeight.addEventListener('submit', function(event){
+    event.preventDefault();
+    calWeight(heightTwo.value, category.value)
+});
+
 function calWeight(height, category) {
-if(height === ''){
-    showError('Please fill the height field', weightRange);
-}else if(isNaN(height * 2)){
-    showError('Only numbers please', heightTwo);
-} else if (bmiCategories.hasOwnProperty(category)) {
+    if(height === ''){
+        showError('Please fill the height field', weightRange);
+    } else if(isNaN(height)){
+        showError('Only numbers please', heightTwo);
+    } else if (bmiCategories.hasOwnProperty(category)) {
         const minBmi = bmiCategories[category].min;
         const maxBmi = bmiCategories[category].max;
         const maxWeight = Math.round(height * height * maxBmi * 100) / 100;
         const minWeight = Math.round(height * height * minBmi * 100) / 100;
         if(minWeight === 0) {
-            showMesaje('max: ' + maxWeight + 'Kg', weightRange)
+            showMesaje(maxWeight + 'Kg.', weightRange)
+            formWeight.reset()
         } else {
-            showMesaje('min: ' + minWeight + 'Kg' + ' | ' + 'max: ' + maxWeight + 'Kg', weightRange)
+            showMesaje(minWeight + 'Kg' + ' - ' + maxWeight + 'Kg', weightRange)
+            formWeight.reset()
         }
     }    
 }
